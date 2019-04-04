60d8e9bbf4e821eab56cb25561cf0aec2821a20b
azrael <azrael@wacaideMacBook-Pro-2.local>
2019-04-04 21:13:52 +0800

    通讯录界面新增indexBar

--- lib/home/contact_page/index.dart
+++ lib/home/contact_page/index.dart
@@ -2,23 +2,112 @@ import 'package:flutter/material.dart';
 
 import './widgets/contact_item.dart';
 
+import '../../constants.dart' show Constants;
 import '../../modal/contact.dart' show Contact, ContactPageData;
+
+const INDEX_BAR_WORDS = [
+  "↑",
+  "☆",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
 
 class ContactsPage extends StatefulWidget {
   _ContactsPageState createState() => _ContactsPageState();
 }
 
 class _ContactsPageState extends State<ContactsPage> {
-  final List<Contact> _contacts = ContactPageData.mock().contacts;
+  final ContactPageData _data = ContactPageData.mock();
+  final List<Contact> _contacts = [];
+  final List<ContactItem> _functionButtons = [
+    ContactItem(avatar: 'assets/images/ic_new_friend.png', title: '新的朋友'),
+    ContactItem(
+      avatar: 'assets/images/ic_group_chat.png',
+      title: '群聊',
+    ),
+    ContactItem(avatar: 'assets/images/ic_tag.png', title: '标签'),
+    ContactItem(avatar: 'assets/images/ic_public_account.png', title: '公众号'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _contacts
+      ..addAll(_data.contacts)
+      ..addAll(_data.contacts)
+      ..addAll(_data.contacts);
+    _contacts
+        .sort((Contact a, Contact b) => a.nameIndex.compareTo(b.nameIndex));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (BuildContext context, int index) {
-        Contact _contact = _contacts[index];
-        return ContactItem(avatar: _contact.avatar, title: _contact.name);
-      },
-      itemCount: _contacts.length,
+    int len = _functionButtons.length;
+    final List<Widget> _letters = INDEX_BAR_WORDS.map((String letter) {
+      return Expanded(
+        child: Text(letter),
+      );
+    }).toList();
+
+    return Stack(
+      children: <Widget>[
+        ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            if (index < len) {
+              return _functionButtons[index];
+            }
+
+            int _contactIndex = index - len;
+            Contact _contact = _contacts[_contactIndex];
+
+            bool _isGroupTitle = true;
+            if (_contactIndex > 0 &&
+                _contact.nameIndex == _contacts[_contactIndex - 1].nameIndex) {
+              _isGroupTitle = false;
+            }
+
+            return ContactItem(
+              avatar: _contact.avatar,
+              title: _contact.name,
+              groupTitle: _isGroupTitle ? _contact.nameIndex : null,
+            );
+          },
+          itemCount: _contacts.length + len,
+        ),
+        Positioned(
+          right: 0.0,
+          top: 0.0,
+          bottom: 0.0,
+          width: Constants.IndexBarWidth,
+          child: Column(
+            children: _letters,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/home/contact_page/widgets/contact_item.dart
+++ lib/home/contact_page/widgets/contact_item.dart
@@ -1,43 +1,72 @@
 import 'package:flutter/material.dart';
 
-import '../../../constants.dart' show Constants, AppColors;
+import '../../../constants.dart' show Constants, AppColors, AppStyles;
 
 class ContactItem extends StatelessWidget {
-  ContactItem({@required this.avatar, @required this.title, this.groupTitle});
+  ContactItem({
+    @required this.avatar,
+    @required this.title,
+    this.groupTitle,
+    this.onPressed,
+  });
 
   final String avatar;
   final String title;
   final String groupTitle;
+  final VoidCallback onPressed;
+
+  bool get isAvatarFromNet {
+    return avatar.startsWith('http') || avatar.startsWith('https');
+  }
 
   @override
   Widget build(BuildContext context) {
-    Widget _avatarIcon = Image.network(
-      avatar,
-      width: Constants.ContactAvatarSize,
-      height: Constants.ContactAvatarSize,
-    );
+    Widget _avatarIcon = isAvatarFromNet
+        ? Image.network(
+            avatar,
+            width: Constants.ContactAvatarSize,
+            height: Constants.ContactAvatarSize,
+          )
+        : Image.asset(
+            avatar,
+            width: Constants.ContactAvatarSize,
+            height: Constants.ContactAvatarSize,
+          );
 
-    return Container(
-      padding: EdgeInsets.only(right: 16.0, left: 16.0),
-      child: Container(
-        padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(
-              width: Constants.DividerWidth,
-              color: const Color(AppColors.DividerColor),
-              style: BorderStyle.solid,
-            ),
+    Widget _button = Container(
+      margin: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: Constants.DividerWidth,
+            color: Color(AppColors.DividerColor),
+            style: BorderStyle.solid,
           ),
         ),
-        child: Row(
-          children: <Widget>[
-            _avatarIcon,
-            SizedBox(width: 10.0),
-            Text(title),
-          ],
-        ),
+      ),
+      child: Row(
+        children: <Widget>[
+          _avatarIcon,
+          SizedBox(width: 10.0),
+          Text(title),
+        ],
       ),
     );
+
+    return groupTitle != null
+        ? Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(16.0, 5.0, 16.0, 5.0),
+                color: const Color(AppColors.ContactGroupTitleBg),
+                alignment: Alignment.centerLeft,
+                child:
+                    Text(groupTitle, style: AppStyles.GroupTitleItemTextStyle),
+              ),
+              _button
+            ],
+          )
+        : _button;
   }
 }
