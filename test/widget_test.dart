d4c78a0329dcb038056566d2f65b57e61a1ec8d4
Azrael <wuyanbin888888@163.com>
2019-04-07 22:03:55 +0800

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:wechat_clone/main.dart';
-import '../lib/pages/home_page.dart';
+import '../lib/pages/home_page/home_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
